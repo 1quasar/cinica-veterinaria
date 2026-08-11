2e30942aea83a47ad07d13cf7b8e3564061a2765
José Victor Ribeiro Lima <res0146848@aluno.firjansenaisesi.com.br>
2026-08-11 19:29:19 -0300

    ClinicaVet

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>Dashboard Administrativo</h1>
+
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card body">
+                    <h5>Animais Cadastrados</h5>
+                    <h2>0</h2>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card body">
+                    <h5>Usuário</h5>
+                    <h2>0</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,7 @@
+<div class="bg-light p-3">
+    <h5>Menu</h5>
+
+    <ul class="nav flex-column">
+        li.nav
+    </ul>
+</div>
